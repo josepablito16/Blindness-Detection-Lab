1db8396de58cdd63c5703461003af2c705508ca7
josepablito16 <josepablocif16@gmail.com>
2020-08-20 10:37:05 -0600

    Se juega un poco con la aumentacion de la data

--- ContrastDeep.py
+++ ContrastDeep.py
@@ -12,20 +12,33 @@ NOMBRE_MODELO='contrastDeep'
 NUM_CLASES=5
 
 # Train
-train_datagen = ImageDataGenerator( rescale = 1.0/255. )
+train_datagen = ImageDataGenerator( 
+	rescale = 1.0/255.,
+	brightness_range=(1,2),
+	rotation_range=180.,
+	shear_range=0.2,
+	horizontal_flip=True,
+	vertical_flip=True,)
 
-train_dir='..\\imagen'
+train_dir='../train'
 
 with tf.device('/gpu:0'):
-
-  train_generator = train_datagen.flow_from_directory(train_dir,
+	train_generator = train_datagen.flow_from_directory(train_dir,
                                                     batch_size=1,
                                                     class_mode='binary',
+                                                    seed=1,
                                                     shuffle=True,
-                                                    target_size=(150, 150))
+                                                    target_size=(474,358))
 
 
-  for item in train_generator:
-    plt.imshow(item[0][0])
-    plt.show()
-    break
+  
+
+	contador=0
+	for item in train_generator:
+		if(contador==5):
+			break
+		plt.imshow(item[0][0])
+		plt.show()	
+		contador+=1
+		
+    
