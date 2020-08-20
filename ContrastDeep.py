78c1e20e4b1aaf77da2983c71b7957e078b70ab1
OJP98 <ojuarez_p@hotmail.com>
2020-08-19 20:00:11 -0600

    Testing de imágenes

--- ContrastDeep.py
+++ ContrastDeep.py
@@ -1,5 +1,12 @@
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import tensorflow as tf
+import numpy as np
+import os
+import PIL
+import PIL.Image
+from PIL import Image
+import matplotlib.pyplot as plt
+
 
 NOMBRE_MODELO='contrastDeep'
 NUM_CLASES=5
@@ -7,43 +14,18 @@ NUM_CLASES=5
 # Train
 train_datagen = ImageDataGenerator( rescale = 1.0/255. )
 
-train_dir='..\\train'
+train_dir='..\\imagen'
 
 with tf.device('/gpu:0'):
 
   train_generator = train_datagen.flow_from_directory(train_dir,
-                                                    batch_size=400,
+                                                    batch_size=1,
                                                     class_mode='binary',
                                                     shuffle=True,
                                                     target_size=(150, 150))
 
 
   for item in train_generator:
-    print(item)
+    plt.imshow(item[0][0])
+    plt.show()
     break
-
-  # model = tf.keras.models.Sequential([
-  #   tf.keras.layers.Conv2D(16, (3,3), activation='relu', input_shape=(150, 150, 3)),
-  #   tf.keras.layers.MaxPooling2D(2,2),
-  #   tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
-  #   tf.keras.layers.MaxPooling2D(2,2), 
-  #   tf.keras.layers.Conv2D(64, (3,3), activation='relu'), 
-  #   tf.keras.layers.MaxPooling2D(2,2),
-  #   # Flatten the results 
-  #   tf.keras.layers.Flatten(),
-  #   # neuron hidden layer
-  #   tf.keras.layers.Dense(512, activation='relu'),
-  #   tf.keras.layers.Dense(NUM_CLASES, activation='softmax')
-  #   ])
-
-
-  # model.compile(optimizer='adam',
-  #               loss = 'sparse_categorical_crossentropy',
-  #               metrics=['accuracy'])
-
-
-  # model.fit(train_generator, epochs=25, verbose=2)
-
-
-  # #GUARDAR MODELO
-  # model.save_weights(NOMBRE_MODELO)
--- Test.py
+++ Test.py
@@ -1,11 +1,11 @@
 import tensorflow as tf
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-NOMBRE_MODELO='prueba'
+NOMBRE_MODELO='grayscale'
 NUM_CLASES=5
 
 #CARGAR MODELO
-model = tf.keras.models.Sequential([tf.keras.layers.Flatten(input_shape=(150, 150,3)), 
+model = tf.keras.models.Sequential([tf.keras.layers.Flatten(input_shape=(150, 150,1)), 
                                     tf.keras.layers.Dense(128, activation=tf.nn.relu), 
                                     tf.keras.layers.Dense(NUM_CLASES, activation=tf.nn.softmax)])
 
@@ -15,17 +15,14 @@ model.load_weights(NOMBRE_MODELO)
 test_datagen  = ImageDataGenerator( rescale = 1.0/255. )
 validation_dir='..\\test'
 validation_generator =  test_datagen.flow_from_directory(validation_dir,
-                                                         batch_size=20,
+                                                         batch_size=400,
                                                          class_mode  = 'binary',
-                                                         target_size = (150, 150))
+                                                         target_size = (150, 150),
+                                                         color_mode='grayscale')
 
-predictions = model.predict(validation_generator)
+model.compile(optimizer='adam',
+            loss = 'sparse_categorical_crossentropy',
+            metrics=['accuracy'])
 
-import numpy as np
-
-for i in predictions:
-	
-	print(i)
-	print(np.argmax(i))
-	print(max(i))
-	print()
+test_loss, test_acc = model.evaluate(validation_generator)
+print(test_acc)
